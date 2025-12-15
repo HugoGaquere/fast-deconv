792e1bf7e328111ed2c114ab16864488b6966f8a
Hugo Gaquere <hugo.gaquere@gmail.com>
2025-12-15 10:43:33 +0100

    Update span types

--- include/fast_deconv/core/span_types.hpp
+++ include/fast_deconv/core/span_types.hpp
@@ -4,28 +4,35 @@
 namespace fast_deconv::core {
 
 template <std::size_t N> using dims = emu::dextents<std::size_t, N>;
-template <typename T, std::size_t N> using mdspan = emu::cuda::device::mdspan<T, dims<N>>;
 
-template <typename T> using device_vect = mdspan<T, 1>;
-template <typename T> using device_span2d = mdspan<T, 2>;
-template <typename T> using device_span3d = mdspan<T, 3>;
-template <typename T> using device_span4d = mdspan<T, 4>;
-template <typename T> using device_span5d = mdspan<T, 5>;
-template <typename T> using device_span6d = mdspan<T, 6>;
+// Layout right
+template <typename T, std::size_t N> using mdspan = emu::cuda::device::mdspan<T, dims<N>, emu::layout_right>;
 
-using device_vect_f   = device_vect<float>;
-using device_span2d_f = device_span2d<float>;
-using device_span3d_f = device_span3d<float>;
-using device_span4d_f = device_span4d<float>;
-using device_span5d_f = device_span5d<float>;
-using device_span6d_f = device_span6d<float>;
+using device_vect_f   = mdspan<float, 1>;
+using device_span2d_f = mdspan<float, 2>;
+using device_span3d_f = mdspan<float, 3>;
+using device_span4d_f = mdspan<float, 4>;
+using device_span5d_f = mdspan<float, 5>;
+using device_span6d_f = mdspan<float, 6>;
 
-using device_vect_b   = device_vect<bool>;
-using device_span2d_b = device_span2d<bool>;
-using device_span3d_b = device_span3d<bool>;
-using device_span4d_b = device_span4d<bool>;
-using device_span5d_b = device_span5d<bool>;
-using device_span6d_b = device_span6d<bool>;
+using device_vect_b   = mdspan<bool, 1>;
+using device_span2d_b = mdspan<bool, 2>;
+using device_span3d_b = mdspan<bool, 3>;
+using device_span4d_b = mdspan<bool, 4>;
+using device_span5d_b = mdspan<bool, 5>;
+using device_span6d_b = mdspan<bool, 6>;
+
+// Layout stride
+template <typename T, std::size_t N> using mdspan_strided = emu::cuda::device::mdspan<T, dims<N>, emu::layout_stride>;
+
+using device_vect_fs   = mdspan_strided<float, 1>;
+using device_span2d_fs = mdspan_strided<float, 2>;
+using device_span3d_fs = mdspan_strided<float, 3>;
+using device_span4d_fs = mdspan_strided<float, 4>;
+using device_span5d_fs = mdspan_strided<float, 5>;
+using device_span6d_fs = mdspan_strided<float, 6>;
+
+
 
 
 }  // namespace fast_deconv::core
--- src/python/fast_deconv.cu
+++ src/python/fast_deconv.cu
@@ -25,6 +25,45 @@ void inspect(const Mdspan& a)
   }
 }
 
+template <class MDS>
+__global__ void print_mdspan_kernel(MDS m)
+{
+  int tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (tid == 0) {
+    constexpr int R = (int)MDS::rank();
+    printf("mdspan: rank=%d\n", R);
+
+    for (int d = 0; d < R; ++d) {
+      printf("extent(%d)=%ld  stride(%d)=%ld\n", d, m.extent(d), d, m.stride(d));
+    }
+  }
+
+  int rows = (int)m.extent(0);
+  int cols = (int)m.extent(1);
+
+  int total = rows * cols;
+  if (tid >= total) return;
+
+  int row = tid / cols;
+  int col = tid % cols;
+
+  auto val = m(row, col);
+
+  printf("tid=%d row=%d col=%d data=%f\n", tid, row, col, (double)val);
+}
+
+template <typename Mdspan>
+void print_mdspan(const Mdspan& ms)
+{
+  fmt::println("{} {} {} {}", ms.extent(0), ms.stride(0), ms.extent(1), ms.stride(1));
+  int total   = ms.size();
+  int threads = 128;
+  int blocks  = (total + threads - 1) / threads;
+  print_mdspan_kernel<<<blocks, threads>>>(ms);
+  cudaDeviceSynchronize();
+}
+
 std::pair<int, float> argmax(const fast_deconv::core::device_span2d_f& data,
                              const fast_deconv::core::device_span2d_b& mask,
                              bool use_abs,
@@ -60,15 +99,24 @@ PYBIND11_MODULE(_fast_deconv, m)
                     &argmax_mdspan,
                     R"pbdoc( A function that find the index of the maximum value.)pbdoc");
 
-  matrix_module.def("subtract", &subtract<fast_deconv::core::device_vect_f>, R"pbdoc( C = A - B)pbdoc");
-  matrix_module.def("subtract", &subtract<fast_deconv::core::device_span2d_f>, R"pbdoc( C = A - B)pbdoc");
-  matrix_module.def("subtract", &subtract<fast_deconv::core::device_span3d_f>, R"pbdoc( C = A - B)pbdoc");
-  matrix_module.def("subtract", &subtract<fast_deconv::core::device_span4d_f>, R"pbdoc( C = A - B)pbdoc");
-  matrix_module.def("subtract", &subtract<fast_deconv::core::device_span5d_f>, R"pbdoc( C = A - B)pbdoc");
-  matrix_module.def("subtract", &subtract<fast_deconv::core::device_span6d_f>, R"pbdoc( C = A - B)pbdoc");
+  matrix_module.def(
+    "subtract", &subtract<fast_deconv::core::device_vect_f>, R"pbdoc( C = A - B)pbdoc");
+  matrix_module.def(
+    "subtract", &subtract<fast_deconv::core::device_span2d_f>, R"pbdoc( C = A - B)pbdoc");
+  matrix_module.def(
+    "subtract", &subtract<fast_deconv::core::device_span3d_f>, R"pbdoc( C = A - B)pbdoc");
+  matrix_module.def(
+    "subtract", &subtract<fast_deconv::core::device_span4d_f>, R"pbdoc( C = A - B)pbdoc");
+  matrix_module.def(
+    "subtract", &subtract<fast_deconv::core::device_span5d_f>, R"pbdoc( C = A - B)pbdoc");
+  matrix_module.def(
+    "subtract", &subtract<fast_deconv::core::device_span6d_f>, R"pbdoc( C = A - B)pbdoc");
 
   py::class_<fast_deconv::core::stream_resources>(m, "stream_resources").def(py::init<>());
 
   m.def("inspect", &inspect<fast_deconv::core::device_vect_f>, R"pbdoc(Inspect)pbdoc");
   m.def("inspect", &inspect<fast_deconv::core::device_span2d_f>, R"pbdoc(Inspect)pbdoc");
+  // m.def("print", &print_mdspan<fast_deconv::core::device_span2d_f>, R"pbdoc(Print)pbdoc");
+  m.def("print", &print_mdspan<fast_deconv::core::device_span2d_fs>, R"pbdoc(Print)pbdoc");
+  m.def("print", &print_mdspan<fast_deconv::core::device_span2d_f>, R"pbdoc(Print)pbdoc");
 }
