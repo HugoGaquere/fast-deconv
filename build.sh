2f8b36695e2a6742c25af51ec79e5973b04412f7
Hugo Gaquere <hugo.gaquere@gmail.com>
2026-02-10 19:00:09 +0100

    big refactor end

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# conan build -b missing
-cmake --preset conan-release # cmake configure with default build type (release)
-cmake --build --preset conan-release # build the library
+export MATHDX_ROOT="/usr/local/nvidia-cuda12/mathdx/25.12/"
+conan build . -b missing
 
-# cmake -S . -B build/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_CUDA_COMPILER=/usr/local/cuda-12.8/bin/nvcc -DCUTENSOR_ROOT=/usr && cmake --build build/
+# conan build -b missing
+# cmake --preset conan-release # cmake configure with default build type (release)
+# cmake --build --preset conan-release # build the library
+
+ # cmake -S . -B build/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_CUDA_COMPILER=/usr/local/cuda-12.9/bin/nvcc -DCUTENSOR_ROOT=/usr && cmake --build build/
