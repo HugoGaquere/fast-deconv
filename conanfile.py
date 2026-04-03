96cb4a662e31b69c53f758eba3356b4dedc0e76b
Hugo Gaquere <hugo.gaquere@gmail.com>
2026-04-03 17:25:26 +0200

    Bump version to 0.1.1

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
 
 class Recipe(ConanFile):
     name = "fast-deconv"
-    version = "0.1.0"
+    version = "0.1.1"
 
     # Keep as it is
     settings = "os", "compiler", "build_type", "arch"
