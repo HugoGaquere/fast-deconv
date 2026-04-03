ec2fd58594f6b1487dc34c545a72feb7db14eab1
Hugo Gaquere <hugo.gaquere@gmail.com>
2026-04-03 11:50:35 +0200

    Add Wscms binding

--- src/python/bindings/wscms.cu
+++ src/python/bindings/wscms.cu
@@ -6,7 +6,6 @@
 #include <fast_deconv/algorithm/wscms_types.hpp>
 #include <fast_deconv/core/span_types.hpp>
 
-#include "fast_deconv/core/resources.hpp"
 #include "fast_deconv_bindings.hpp"
 
 namespace py = pybind11;
@@ -19,26 +18,6 @@ void bind_wscms(py::module_& m)
 {
   auto wscms_module = m.def_submodule("wscms", "WSCMS module");
 
-  py::class_<wscms::WSCMS_params>(wscms_module, "WscmsParams")
-      .def(py::init<bool, bool, bool, float, int, int, float>(), py::arg("beam_enable"),
-           py::arg("do_abs"), py::arg("per_scale_mask"), py::arg("peak_factor"),
-           py::arg("max_subminor_iter"), py::arg("n_scales"), py::arg("padding"))
-      .def_readwrite("beam_enable", &wscms::WSCMS_params::beam_enable)
-      .def_readwrite("do_abs", &wscms::WSCMS_params::do_abs)
-      .def_readwrite("per_scale_mask", &wscms::WSCMS_params::per_scale_mask)
-      .def_readwrite("peak_factor", &wscms::WSCMS_params::peak_factor)
-      .def_readwrite("max_subminor_iter", &wscms::WSCMS_params::max_subminor_iter)
-      .def_readwrite("n_scales", &wscms::WSCMS_params::n_scales)
-      .def_readwrite("padding", &wscms::WSCMS_params::padding);
-
-  py::class_<wscms::WSCMS_ctx>(wscms_module, "WscmsCtx")
-      .def(py::init<core::device_span4d<float>, core::device_span2d<float>,
-                    core::device_vect<float>, core::device_span2d<bool>, core::device_vect<float>,
-                    core::host_vect<float>, core::host_span2d<int>, core::host_span2d<float>>(),
-           py::arg("jones_norm"), py::arg("xdes"), py::arg("weights_freq"), py::arg("scale_masks"),
-           py::arg("scale_sigmas"), py::arg("scale_bias"), py::arg("map_pixel_facet"),
-           py::arg("gains"));
-
   py::class_<wscms::sky_component>(wscms_module, "SkyComponent")
       .def_readonly("row", &wscms::sky_component::row)
       .def_readonly("col", &wscms::sky_component::col)
@@ -46,8 +25,29 @@ void bind_wscms(py::module_& m)
       .def_readonly("gain", &wscms::sky_component::gain)
       .def_readonly("coeffs", &wscms::sky_component::coeffs);
 
-  wscms_module.def("run_wscms", &wscms::run_wscms, py::arg("dirty"), py::arg("mean_residual"),
-                   py::arg("psfs"), py::arg("psfs_2"), py::arg("ctx"), py::arg("params"),
-                   R"pbdoc( Run the full WSCMS algorithm )pbdoc");
+  py::class_<wscms::Wscms>(wscms_module, "Wscms")
+      .def(py::init<const core::device_span6d<float>&, const core::device_span4d<float>&,
+                    const core::device_span4d<float>&, const core::device_span2d<float>&,
+                    const core::device_vect<float>&, const core::device_span2d<bool>&,
+                    const core::device_vect<float>&, const core::host_vect<float>&,
+                    const core::host_span2d<int>&, const core::host_span2d<float>&, int, int, float,
+                    bool, float, int>(),
+           py::arg("psfs"), py::arg("psfs_2"), py::arg("jones_norm"), py::arg("xdes"),
+           py::arg("weights_freq"), py::arg("scale_masks"), py::arg("scale_sigmas"),
+           py::arg("scale_bias"), py::arg("map_pixel_facet"), py::arg("gains"),
+           py::arg("dirty_nrows"), py::arg("dirty_ncols"), py::arg("peak_factor"),
+           py::arg("clean_negative"), py::arg("fft_padding"), py::arg("exec_device") = 0,
+           py::keep_alive<1, 2>(),   // psfs
+           py::keep_alive<1, 3>(),   // psfs_2
+           py::keep_alive<1, 4>(),   // jones_norm
+           py::keep_alive<1, 5>(),   // xdes
+           py::keep_alive<1, 6>(),   // weights_freq
+           py::keep_alive<1, 7>(),   // scale_masks
+           py::keep_alive<1, 8>(),   // scale_sigmas
+           py::keep_alive<1, 9>(),   // scale_bias
+           py::keep_alive<1, 10>(),  // map_pixel_facet
+           py::keep_alive<1, 11>())  // gains
+      .def("run", &wscms::Wscms::run, py::arg("dirty"), py::arg("mean_residual"),
+           py::arg("max_iterations"), R"pbdoc(Run the WSCMS algorithm)pbdoc");
 }
 }  // namespace fast_deconv::python
