b53f91221876611da051d121040ed2d74c3e9281
Hugo Gaquere <hugo.gaquere@gmail.com>
2026-07-01 11:10:21 +0200

    chore: remove dead header-only detail/ headers and stale references

--- src/main.cu
+++ src/main.cu
@@ -1,20 +1,10 @@
-// #include <fast_deconv/core/access_policy.hpp>
-// #include <fast_deconv/core/concepts.hpp>
-// #include <fast_deconv/core/dispatcher.hpp>
-// #include <fast_deconv/core/kernel_traits.hpp>
 #include <fast_deconv/core/resources.hpp>
 #include <fast_deconv/core/span_types.hpp>
 
-// #include <fast_deconv/matrix/argmax.hpp>
-// #include <fast_deconv/matrix/subtract.hpp>
-
-// #include <fast_deconv/linalg/pinv.cuh>
 #include <fmt/base.h>
 
 #include <fast_deconv/algorithm/wscms.hpp>
 #include <fast_deconv/algorithm/wscms_types.hpp>
-
-// #include <fast_deconv/algorithm/detail/wscms_minor_loop.cuh>
 
 int main(int argc, char** argv)
 {
